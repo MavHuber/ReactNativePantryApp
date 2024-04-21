f5f72d76d752bbcb2e6535f9193b3e61056002f3
MavHuber <mlh5401056@gmail.com>
2024-04-21 18:32:04 -0400

    Improve Quote of the Day on HomeScreen

--- RN-Pantry-Assistant/navigation/screens/HomeScreen.js
+++ RN-Pantry-Assistant/navigation/screens/HomeScreen.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import { View, Text, Button, ImageBackground, StyleSheet, Dimensions, Animated } from 'react-native';
+import { View, Text, Button, ImageBackground, StyleSheet, Dimensions, Animated, TouchableOpacity } from 'react-native';
+
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 
 export default function HomeScreen({navigation}) {
     return(
@@ -12,8 +16,36 @@ export default function HomeScreen({navigation}) {
                 <View>
                     <Text style={styles.heading}>Welcome, friend!</Text> 
                 </View>
-                <View style={styles.quote}>
-                    <Text>Quote</Text>
+                <View style={styles.quoteSpace}>
+                    <Text style={styles.quoteTitle}>
+                        Quote of the Day
+                    </Text>
+                    <FontAwesome5 name="quote-left" style={{fontSize:20,marginBottom:-12}} color="#000" />
+                    <Text style={styles.quote}>
+                        You can't have a rainbow without a little rain.
+                    </Text>
+                    <FontAwesome5 name="quote-right" style={{fontSize:20, marginTop:-20,marginBottom:20}} color="#000" />
+                    <Text 
+                        style={{
+                            textAlign: 'right', 
+                            fontWeight: '300', 
+                            fontStyle:'italic', 
+                            fontSize: 16, 
+                            color: '#000'
+                            }}>
+                                --- Author Name
+                            </Text>
+                    <TouchableOpacity onPress={() => {}} style={{backgroundColor: 'blue', padding: 20, borderRadius: 30, marginVertical: 20,}}>
+                        <Text style={{color:'#fff', fontSize: 18, textAlign: 'center'}}>
+                            New Quote
+                        </Text>
+                    </TouchableOpacity>
+
+                    <View style={{flexDirection:'row'}}>
+                        <TouchableOpacity>
+                            <FontAwesome
+                        </TouchableOpacity>
+                    </View>
 
                 </View>
             </ImageBackground>
@@ -42,11 +74,28 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         padding: 10,
     },
-    quote: {
+    quoteSpace: {
         width: '90%',
         backgroundColor: '#fff',
         borderRadius: 20,
         padding: 5,
 
-    }
+    },
+    quoteTitle: {
+        textAlign: 'center',
+        fontSize: 26,
+        fontWeight: '600',
+        color: '#333',
+        marginBottom: 20,
+    },
+    quote: {
+        color: '#000',
+        fontSize: 16,
+        lineHeight: 26,
+        letterSpacing: 1.1,
+        fontWeight: '400',
+        textAlign: 'center',
+        marginBottom: 10,
+        paddingHorizontal: 30,
+    },
 })
